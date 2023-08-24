218dcbc3921e807929db689278d2d702446608fc
Vladislav Sergeev <vladismann@yandex.ru>
2023-08-24 12:49:40 +0300

    feat: statistics order

--- stats/service/src/main/java/ru/practicum/service/enpointHit/repo/EndpointHitRepo.java
+++ stats/service/src/main/java/ru/practicum/service/enpointHit/repo/EndpointHitRepo.java
@@ -12,29 +12,33 @@ import java.util.List;
 public interface EndpointHitRepo extends JpaRepository<EndpointHit, Long> {
 
 
-    @Query("SELECT new ru.practicum.dto.EndpointHitDto.GetStatsDto(h.app, h.uri, count(Distinct(h.ip))) " +
+    @Query("SELECT new ru.practicum.dto.EndpointHitDto.GetStatsDto(h.app, h.uri, count(Distinct(h.ip)) as hits) " +
             "from EndpointHit h " +
             "WHERE h.uri IN :uris " +
             "AND h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.uri, h.app")
+            "GROUP BY h.uri, h.app " +
+            "ORDER BY hits desc")
     List<GetStatsDto> findAllByTimestampBetweenAndUrisInDistinct(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 
-    @Query("SELECT new ru.practicum.dto.EndpointHitDto.GetStatsDto(h.app, h.uri, count(h.ip)) " +
+    @Query("SELECT new ru.practicum.dto.EndpointHitDto.GetStatsDto(h.app, h.uri, count(h.ip) as hits) " +
             "from EndpointHit h " +
             "WHERE h.uri IN :uris " +
             "AND h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.uri, h.app")
+            "GROUP BY h.uri, h.app " +
+            "ORDER BY hits desc")
     List<GetStatsDto> findAllByTimestampBetweenAndUrisIn(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end, @Param("uris") List<String> uris);
 
-    @Query("SELECT new ru.practicum.dto.EndpointHitDto.GetStatsDto(h.app, h.uri, count(Distinct(h.ip))) " +
+    @Query("SELECT new ru.practicum.dto.EndpointHitDto.GetStatsDto(h.app, h.uri, count(Distinct(h.ip)) as hits) " +
             "from EndpointHit h " +
             "WHERE h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.uri, h.app")
+            "GROUP BY h.uri, h.app " +
+            "ORDER BY hits desc")
     List<GetStatsDto> findAllByTimestampBetweenDistinct(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.dto.EndpointHitDto.GetStatsDto(h.app, h.uri, count(h.ip)) " +
+    @Query("SELECT new ru.practicum.dto.EndpointHitDto.GetStatsDto(h.app, h.uri, count(h.ip) as hits) " +
             "from EndpointHit h " +
             "WHERE h.timestamp BETWEEN :start AND :end " +
-            "GROUP BY h.uri, h.app")
+            "GROUP BY h.uri, h.app " +
+            "ORDER BY hits desc")
     List<GetStatsDto> findAllByTimestampBetween(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 }
